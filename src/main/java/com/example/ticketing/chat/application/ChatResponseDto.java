8f2070ee60c6fc810060ab3b38b95fe93e05f5e4
jeongho <pjhpjh0805@naver.com>
2025-04-12 08:54:22 +0900

    feat: 채팅 Redis 관련 클래스 및 컴포넌트 제거

--- src/main/java/com/example/ticketing/chat/application/ChatResponseDto.java
+++ src/main/java/com/example/ticketing/chat/application/ChatResponseDto.java
@@ -1,6 +1,5 @@
 package com.example.ticketing.chat.application;
 
-import com.example.ticketing.chat.ChatTemp;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -14,13 +13,4 @@ public class ChatResponseDto {
     private String name;
     private String text;
     private LocalDateTime sendAt;
-
-    public static ChatResponseDto createFromChat(ChatTemp chatTemp) {
-        ChatResponseDto dto = new ChatResponseDto();
-        dto.key = chatTemp.getKey();
-        dto.name = chatTemp.getName();
-        dto.text = chatTemp.getText();
-        dto.sendAt = chatTemp.getCreatedAt();
-        return dto;
-    }
 }
--- src/main/java/com/example/ticketing/chat/infra/ChatRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.ticketing.chat.infra;
-
-import com.example.ticketing.chat.ChatTemp;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ChatRepository extends CrudRepository<ChatTemp, Long> {}
