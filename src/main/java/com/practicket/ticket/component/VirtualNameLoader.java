ccdf537fa7a07ad89e1bf50c0b01fddf7a76eb15
jeongho <pjhpjh0805@naver.com>
2025-12-13 18:45:08 +0900

    fix: jar 파일 실행시 classpath 에러 수정

--- src/main/java/com/practicket/ticket/component/VirtualNameLoader.java
+++ src/main/java/com/practicket/ticket/component/VirtualNameLoader.java
@@ -2,22 +2,28 @@ package com.practicket.ticket.component;
 
 import jakarta.annotation.PostConstruct;
 import lombok.Getter;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.io.InputStreamReader;
 import java.util.List;
 
 @Getter
 @Component
 public class VirtualNameLoader {
 
+    @Value("classpath:etc/names.txt")
+    private Resource nameResource;
+
     private List<String> names;
 
     @PostConstruct
     public void load() throws IOException {
-        ClassPathResource resource = new ClassPathResource("etc/names.txt");
-        names = Files.readAllLines(resource.getFile().toPath());
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(nameResource.getInputStream()))) {
+            names = reader.lines().toList();
+        }
     }
 }
