2e7f0b429c1ed8e8a61c26902ced53eda6d91ae2
jeongho <qkrwjdgh1721@gmail.com>
2026-06-04 22:17:41 +0900

    fix: ArtUpdateRequest에서 알 수 없는 JSON 필드 무시 처리
    PRACTICKET-4T: 클라이언트가 width 등 미정의 필드를 포함해 PUT /api/arts/{artId} 요청 시 UnrecognizedPropertyException 발생

--- src/main/java/com/practicket/art/dto/ArtUpdateRequest.java
+++ src/main/java/com/practicket/art/dto/ArtUpdateRequest.java
@@ -1,10 +1,12 @@
 package com.practicket.art.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ArtUpdateRequest {
 
     @NotBlank(message = "제목을 입력해주세요.")
